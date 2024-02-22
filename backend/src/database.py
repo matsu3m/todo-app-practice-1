fb904c8811f02dfb67f87c375216f63f6dfd2fb6
matsu3m <105341123+matsu3m@users.noreply.github.com>
2024-02-22 14:23:36 +0000

    DynamoDB データのシリアライズ・シリアライズ用の関数を追加

--- backend/src/database.py
+++ backend/src/database.py
@@ -1,8 +1,30 @@
+from typing import Any, Dict, List
+
 import boto3
+from boto3.dynamodb.types import TypeDeserializer, TypeSerializer
 from fastapi import Depends
 
 from src.config import Settings, get_settings
 
+serializer = TypeSerializer()
+deserializer = TypeDeserializer()
+
 
 def get_db_client(settings: Settings = Depends(get_settings)):
     return boto3.client("dynamodb", endpoint_url=settings.local_db_uri if settings.env == "local" else None)
+
+
+def serialize_item(item: Dict[str, Any]):
+    return {k: serializer.serialize(v) for k, v in item.items()}
+
+
+def serialize_items(items: List[Dict[str, Any]]):
+    return [serialize_item(item) for item in items]
+
+
+def deserialize_item(item: Dict[str, Any]):
+    return {k: deserializer.deserialize(v) for k, v in item.items()}
+
+
+def deserialize_items(items: List[Dict[str, Any]]):
+    return [deserialize_item(item) for item in items]
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,7 @@ from fastapi import Depends, FastAPI
 from mypy_boto3_dynamodb.client import DynamoDBClient
 
 from src.config import Settings, get_settings
-from src.database import get_db_client
+from src.database import deserialize_items, get_db_client
 
 app = FastAPI()
 
@@ -16,4 +16,4 @@ async def health_check():
 def get_all_todo_items(db_client: DynamoDBClient = Depends(get_db_client), settings: Settings = Depends(get_settings)):
     response = db_client.scan(TableName=settings.table_name)
     items = response.get("Items", [])
-    return items
+    return deserialize_items(items)
