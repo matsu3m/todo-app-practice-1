5ab782bcf93129b5d210efa19f53055d7cf9a175
matsu3m <105341123+matsu3m@users.noreply.github.com>
2024-02-22 11:55:35 +0000

    Python のリンタ・フォーマッタの設定

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,9 +1,14 @@
 #/bin/bash
 
-sudo chown vscode ./node_modules ./frontend/node_modules
+sudo chown vscode ./node_modules ./frontend/node_modules ./backend/.venv
 
 npm install
 
 cd ./frontend
 npm install
+cd ..
+
+cd ./backend
+poetry config virtualenvs.in-project true
+poetry install --no-root
 cd ..
---
+++ backend/src/tmp.py
@@ -0,0 +1,3 @@
+def tmp():
+
+    return True
