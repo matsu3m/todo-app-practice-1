1d95020f6c4dc81e68f1744a4d3118d70e342fd5
matsu3m <105341123+matsu3m@users.noreply.github.com>
2024-02-29 14:25:45 +0000

    ドラッグアンドドロップによるステータス変更

--- frontend/src/features/todo/components/parts/ToDoLane.tsx
+++ frontend/src/features/todo/components/parts/ToDoLane.tsx
@@ -1,27 +1,32 @@
-import ToDoCard from "@/src/features/todo/components/parts/ToDoCard";
-import { ToDo } from "@/src/features/todo/types";
 import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
+import { useDroppable } from "@dnd-kit/core";
+import { SortableContext, rectSortingStrategy } from "@dnd-kit/sortable";
+import { ToDo } from "../../types";
+import ToDoCard from "./ToDoCard";
 
 type Props = {
-  statusId: string;
-  statusDisplayName: string;
+  laneId: string;
+  laneDisplayName: string;
   todos: ToDo[];
   setTodos: React.Dispatch<React.SetStateAction<ToDo[]>>;
 };
 
-const ToDoLane = ({ statusId, statusDisplayName, todos, setTodos }: Props) => {
+const ToDoLane = ({ laneId, laneDisplayName, todos, setTodos }: Props) => {
+  const { setNodeRef } = useDroppable({ id: laneId });
   return (
-    <Card id={statusId} backgroundColor="gray.50">
-      <CardHeader>
-        <Heading size="sm">{statusDisplayName}</Heading>
-      </CardHeader>
+    <SortableContext id={laneId} items={todos} strategy={rectSortingStrategy}>
+      <Card backgroundColor="gray.50" width={80} ref={setNodeRef}>
+        <CardHeader>
+          <Heading size="sm">{laneDisplayName}</Heading>
+        </CardHeader>
 
-      <CardBody>
-        {todos.map((todo) => (
-          <ToDoCard key={todo.id} todo={todo} setTodos={setTodos} />
-        ))}
-      </CardBody>
-    </Card>
+        <CardBody>
+          {todos.map((todo) => (
+            <ToDoCard key={todo.id} id={todo.id} todo={todo} setTodos={setTodos} />
+          ))}
+        </CardBody>
+      </Card>
+    </SortableContext>
   );
 };
 
