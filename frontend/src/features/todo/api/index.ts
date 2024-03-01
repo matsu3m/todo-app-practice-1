7f29e3d0e09513ff83c64ced4d58ab040b3fcb8f
matsu3m <105341123+matsu3m@users.noreply.github.com>
2024-03-01 02:52:49 +0000

    filteredTodos を state として管理するのをやめた

--- frontend/src/features/todo/api/index.ts
+++ frontend/src/features/todo/api/index.ts
@@ -1,15 +1,9 @@
-import { ToDo } from "../types";
+import { ToDo } from "@/src/features/todo/types";
 
-export const getAllTodos = async (): Promise<{ success: true; data: ToDo[] } | { success: false }> => {
-  try {
-    const response = await fetch("/api/todos/");
-    if (!response.ok) {
-      throw new Error("Response was not ok");
-    }
-    const data = await response.json();
-    return { success: true, data };
-  } catch (e) {
-    console.error(e);
-    return { success: false };
+export const getAllTodos = async (signal?: AbortSignal): Promise<ToDo[]> => {
+  const response = await fetch("/api/todos/", { signal });
+  if (!response.ok) {
+    throw new Error(await response.text());
   }
+  return response.json();
 };
--- frontend/src/features/todo/components/parts/SearchBox.tsx
+++ frontend/src/features/todo/components/parts/SearchBox.tsx
@@ -1,37 +1,14 @@
 import { SearchIcon } from "@chakra-ui/icons";
 import { Flex, Input } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { ToDo } from "../../types";
 
 type Props = {
-  todos: ToDo[];
-  setFilteredTodos: React.Dispatch<React.SetStateAction<ToDo[]>>;
+  setSearchQuery: React.Dispatch<React.SetStateAction<string>>;
 };
-
-const SearchBox = ({ todos, setFilteredTodos }: Props) => {
-  const [searchQuery, setSearchQuery] = useState("");
-
-  useEffect(() => {
-    const filterTodos = () => {
-      const filtered = todos.filter(
-        (todo) =>
-          todo.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
-          todo.description?.toLowerCase().includes(searchQuery.toLowerCase()),
-      );
-      setFilteredTodos(filtered);
-    };
-    filterTodos();
-  }, [searchQuery, todos, setFilteredTodos]);
-
+const SearchBox = ({ setSearchQuery }: Props) => {
   return (
-    <Flex alignItems="center" height={10} padding={2} gap={2} borderRadius={8} border="gray.100" borderWidth={1}>
+    <Flex alignItems="center" height={10} padding={2} gap={2} borderRadius={8} borderColor="gray.100" borderWidth={1}>
       <SearchIcon />
-      <Input
-        placeholder="キーワード検索"
-        value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        variant="unstyled"
-      />
+      <Input placeholder="キーワード検索" onChange={(e) => setSearchQuery(e.target.value)} variant="unstyled" />
     </Flex>
   );
 };
